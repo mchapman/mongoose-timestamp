12ae5263615569195eab528ae17370cae5b0f73b
Mark Chapman <mark.chapman@gmail.com>
2017-03-11 15:20:06 +0000

    Remove old merge detritus

--- index.js
+++ index.js
@@ -5,33 +5,6 @@
  * MIT Licensed
  */
 
-// <<<<<<< HEAD
-// function timestampsPlugin(schema, options) {
-//   var timeOptions = options || {};
-//   if (timeOptions.updatedElement) {timeOptions.updatedElement.type = Date} else {timeOptions.updatedElement = {type: Date}}
-//   if (timeOptions.createdElement) {timeOptions.createdElement.type = Date} else {timeOptions.createdElement = {type: Date}}
-//   if (schema.path('_id')) {
-//     schema.add({
-//       updatedAt: timeOptions.updatedElement
-//     });
-//     schema.virtual('createdAt')
-//       .get( function () {
-//         if (this._createdAt) return this._createdAt;
-//         return this._createdAt = this._id.getTimestamp();
-//       });
-//     schema.pre('save', function (next) {
-//       if (this.isNew) {
-//         this.updatedAt = this.createdAt;
-//       } else {
-//         this.updatedAt = new Date;
-//       }
-//       next();
-//     });
-//   } else {
-//     schema.add({
-//         createdAt: timeOptions.createdElement
-//       , updatedAt: timeOptions.updatedElement
-// =======
 var defaults = require('defaults');
 
 function timestampsPlugin(schema, options) {
